f8fdff6fec190ed9dfc80f5231556ddc84e498bc
Epic91 <hernandeze791@gmail.com>
2021-01-19 02:07:09 -0600

    updates

---
+++ app/controllers/genres_controller.rb
@@ -0,0 +1,36 @@
+class GenresController < ApplicationController
+    #create update and show
+    def index
+        @genres = Genre.all
+    end
+    
+    def show 
+        @genre = Genre.find(params[:id]) 
+    end
+    
+    def new
+        @genre = Genre.new
+    end
+    
+    def create
+        @genre = Genre.new(genre_params)
+        @genre.save
+        redirect_to genre_path(@genre)
+    end
+    
+    def edit
+        @genre = Genre.find(params[:id]) 
+    end
+    
+    def update
+        @genre = Genre.find(params[:id]) 
+        @genre.update(song_params)
+        redirect_to genre_path(@genre)
+    end
+    
+    private
+    
+    def genre_params(*args)
+        params.require(:genre).permit(*args)
+    end
+end
